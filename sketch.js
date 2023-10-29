91be51c90285680e6afe38918baa70e8c9cb1729
cristinamedrom <cristinamedranoroman@gmail.com>
2023-10-29 12:20:27 +0100

    movimiento listo

--- sketch.js
+++ sketch.js
@@ -1,5 +1,30 @@
+let movX = 175;
+let movY = 175;
+
+
 function setup() {
     createCanvas(400, 400);
     fill(255, 0, 0);
-    rect(50, 50, 50, 50);
+    rect(movX, movY, 50, 50);
+}
+
+function draw() {
+
+}
+
+
+function keyPressed() {
+    if (keyCode === UP_ARROW) {
+        movY -= 5;
+    } else if (keyCode === DOWN_ARROW) {
+        movY += 5;
+    } else if (keyCode === LEFT_ARROW) {
+        movX -= 5;
+    } else if (keyCode === RIGHT_ARROW) {
+        movX += 5;
+    }
+    
+    background(255);
+
+    rect(movX, movY, 50, 50);
 }
